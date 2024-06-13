a8ddce43f909e107f1241d40a72679c77bc38ebe
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-06-13 15:29:27 +0000

    Add Syllabus table

--- sheldon/settings.py
+++ sheldon/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kjk$4%qowtpxaxqi+hkgyro*klf+c2x!!v((lhlulbr40=5d)-'
+# DELETE THIS BEFORE SUBMITTING: SECRET_KEY = 'django-insecure-kjk$4%qowtpxaxqi+hkgyro*klf+c2x!!v((lhlulbr40=5d)-'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
