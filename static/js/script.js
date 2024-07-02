7a588462b9623b9e706fd6e9ca0b22a8e9d8290f
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-07-02 14:13:43 +0000

    add fetch to js, not working

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,15 @@
-console.log("Life, The Universe and Everything!");
+console.log("The java script file says hello!");
+
+fetch('https://8000-gustafenebog-sheldon-snyii1xvpkq.ws.codeinstitute-ide.net/', {
+    method: 'POST',
+    headers: {
+        'Content-Type': "application/json"
+    },
+    body: JSON.stringify({
+        name: 'User 1'
+    })
+}).then(res => {
+    return res.json()
+})
+.then(data => console.log(data))
+.catch(error => console.log('ERROR'))
