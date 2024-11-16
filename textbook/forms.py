d2fba07c8dbc76214ff950a861a8306763e42222
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-11-16 18:22:12 +0000

    Style button note_edit as create, view note

--- textbook/forms.py
+++ textbook/forms.py
@@ -5,3 +5,9 @@ class NoteForm(forms.ModelForm):
     class Meta:
         model = Note
         fields = ['body']
+        labels = {
+            'body': '',  # This removes the label for the 'body' field
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={'placeholder': 'Enter your note content here'}),  # Optional: Add a placeholder
+        }
